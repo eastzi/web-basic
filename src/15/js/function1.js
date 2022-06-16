7cde23e3398aa80a3d070aa09eda0614fcda4fe7
eastzi <zero7038@naver.com>
2022-06-16 21:53:36 +0900

    6/16 papago 완성, css생성자, javascript

---
+++ src/15/js/function1.js
@@ -0,0 +1,27 @@
+function add(x, y) {
+    return x + y;
+}
+
+let result = add(10, 20);
+
+console.log(result);
+
+let result2 = function (x, y) {
+    return x + y;
+}
+
+console.log(result2(10, 20));
+
+console.log(function (x, y) {
+    return x + y;
+}(10, 20));
+
+let result3 = (x, y) => {
+    return x + y; 
+}
+
+console.log(result3(20, 30));
+
+let result4 = (x, y) => x + y; 
+
+console.log(result3(20, 30));
